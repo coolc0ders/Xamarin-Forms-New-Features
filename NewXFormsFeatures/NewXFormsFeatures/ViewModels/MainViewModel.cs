1a8b16ba9ae54ec600f8c9418dd7a782c83c4086
Damien <damientohin@gmail.com>
2018-07-28 21:44:53 +0100

    I added additional features

--- NewXFormsFeatures/NewXFormsFeatures/ViewModels/MainViewModel.cs
+++ NewXFormsFeatures/NewXFormsFeatures/ViewModels/MainViewModel.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NewXFormsFeatures.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ReactiveObject
     {
+        string _spanText;
+        public string SpanText
+        {
+            get { return _spanText; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _spanText, value);
+            }
+        }
+        public ReactiveCommand SpanTappedCommand { get; set; }
+
+        public MainViewModel()
+        {
+            SpanText = "Span text...";
+            SpanTappedCommand = ReactiveCommand.Create(async () =>
+            {
+                await App.Current.MainPage.DisplayAlert("Span Tapped", "You tapped the span", "cancel");
+            });
+        }
     }
 }
