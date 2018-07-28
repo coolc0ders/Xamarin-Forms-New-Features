d91d7c527d3d6bca7a5885705e5b3a6aa559fd8b
Damien <damientohin@gmail.com>
2018-07-28 17:40:54 +0100

    I added a few news 3.1 features

---
+++ NewXFormsFeatures/NewXFormsFeatures/Views/TextsModifications.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewXFormsFeatures.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TextsModifications : ContentPage
+	{
+		public TextsModifications ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
